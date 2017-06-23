b03641157097865b12318c26c68f9817675c2214
KyoLuma <couchotcynthia99@gmail.com>
2017-06-23 13:49:53 +0200

    checkpoint

---
+++ tests/AppBundle/Controller/FormController.php
@@ -0,0 +1,8 @@
+<?php
+
+use AppBundle\Entity\Form;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FormController extends Controller {
+    
+}
---
+++ tests/AppBundle/Entity/Form.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class Form{
+
+    protected $name;
+    protected $quantity;
+    protected $price;
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $quantity
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
